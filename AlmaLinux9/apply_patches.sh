13b1d1bd9c3d60c383b7012634f0f4025e9ba0bb
Karol Krizka <kkrizka@gmail.com>
2025-01-07 11:53:27 +0000

    Add force option and check that SPACK_ROOT is set in apply_paches.

--- AlmaLinux9/apply_patches.sh
+++ AlmaLinux9/apply_patches.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
+function usage() {
+    echo "usage: ${0} [-f] /path/to/repo"
+    echo ""
+    echo "Options"
+    echo " -f: Force patch application if ref check fails."
+}
+
+# Get steering options
+OPTSTRING=":f"
+
+FORCE=0 # Do not force
+
+while getopts ${OPTSTRING} opt; do
+    case ${opt} in
+        f)
+            FORCE=1
+            ;;
+        ?)
+            echo "Invalid option: -${OPTARG}"
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 # Get file to patch
 if [ ${#} != 1 ]; then
-    echo "usage: ${0} /path/to/repo"
+    usage
     exit 1
 fi
 
-REPO=${1}
+REPO=$(realpath ${1})
 
 # Determine what commit of spack we have
+if [ -z "${SPACK_ROOT+x}" ]; then
+    echo "No spack detected."
+    exit 1
+fi
 cd ${SPACK_ROOT}
 SPACK_COMMIT=$(git rev-parse HEAD)
 
@@ -19,8 +49,10 @@ if [ "${SPACK_COMMIT}" != "${SPACK_COMMIT_REPO}" ]; then
     echo "Spack version not officially tested."
     echo " recommended version: ${SPACK_COMMIT_REPO}"
     echo " our version: ${SPACK_COMMIT}"
-    echo "Ignoring patches..."
-    exit 0
+    if [ ${FORCE} == 0 ]; then
+        echo "Ignoring patches..."
+        exit 1
+    fi
 fi
 
 # Apply the patches to spack
